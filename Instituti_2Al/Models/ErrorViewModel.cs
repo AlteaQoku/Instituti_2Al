99125207bfa03120b3a64cb384cb37b48445e1aa
Altea Qoku <altea.qoku@fshnstudent.info>
2023-04-22 20:52:06 +0200

    Add project files.

---
+++ Instituti_2Al/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Instituti_2Al.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
